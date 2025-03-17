51684213be2601e53559de341fd29f12c23b21a1
Vichers <lozex283@gmail.com>
2025-03-17 17:53:31 +0300

    Выполнил тз 10-го проекта

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,40 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    private final int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Duration duration;
+
+    public Film() {
+        id = -1;
+    }
+
+    public Film(Film film, int id) {
+        this.id = id;
+        this.name = film.getName();
+        this.description = film.getDescription();
+        this.releaseDate = film.getReleaseDate();
+        this.duration = film.getDuration();
+    }
+
+    @Autowired
+    public void setDuration(int duration) {
+        this.duration = Duration.ofMinutes(duration);
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
 }
